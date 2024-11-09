a36e9631fa9f468a93af09c3ada01ce260415d64
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-09 10:05:56 -0300

    feat: window title
    change window title when button is clicked

---
+++ basic_concepts/signals_and_slots_3.py
@@ -0,0 +1,50 @@
+from PySide6.QtWidgets import QMainWindow, QPushButton, QApplication
+
+from random import choice
+
+window_titles = [
+    "My App",
+    "My App",
+    "Still My App",
+    "Still My App",
+    "What on earth",
+    "What on earth",
+    "This is surprising",
+    "This is surprising",
+    "Something went wrong",
+]
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+
+        self.windowTitleChanged.connect(self.the_window_title_changed)
+
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        print("Clicked")
+        new_window_title = choice(window_titles) 
+        print(f"Setting title: {new_window_title}")
+
+        self.setWindowTitle(new_window_title)
+
+    def the_window_title_changed(self, window_title):
+        print(f"Window title changed: {window_title}")
+
+        if window_title == "Something went wrong":
+            self.button.setDisabled(True)
+
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()
