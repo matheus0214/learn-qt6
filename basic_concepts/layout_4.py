fb8a47bce16e46feaf23f67559f07ce431d2c134
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-13 17:09:58 -0300

    feat: nested layouts

---
+++ basic_concepts/layout_4.py
@@ -0,0 +1,43 @@
+from PySide6.QtWidgets import QApplication, QHBoxLayout, QMainWindow, QVBoxLayout, QWidget
+
+from layout_colorwidget import Color
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        layout1 = QHBoxLayout()
+        layout2 = QVBoxLayout()
+        layout3 = QVBoxLayout()
+
+        layout2.addWidget(Color("red"))
+        layout2.addWidget(Color("yellow"))
+        layout2.addWidget(Color("purple"))
+
+        layout1.addLayout(layout2)
+        layout1.addWidget(Color("green"))
+
+        layout3.addWidget(Color("red"))
+        layout3.addWidget(Color("purple"))
+
+        layout1.addLayout(layout3)
+
+        layout1.setContentsMargins(0, 0, 0, 0)
+        layout1.setSpacing(10)
+        
+        widget = QWidget()
+        widget.setLayout(layout1)
+
+
+        self.setCentralWidget(widget)
+
+
+app = QApplication()
+
+window = MainWindow()
+window.show()
+
+app.exec()
