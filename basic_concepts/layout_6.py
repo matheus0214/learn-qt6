5f6f2ab004212dd9d85c6a4a17cd69dffd833db3
Matheus G. Dias <matheusgiga123@gmail.com>
2024-11-15 10:38:06 -0300

    feat: create a grid layout

---
+++ basic_concepts/layout_6.py
@@ -0,0 +1,31 @@
+from PySide6.QtWidgets import QApplication, QGridLayout, QMainWindow, QWidget
+
+from layout_colorwidget import Color
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        layout = QGridLayout()
+
+        layout.addWidget(Color("red"), 0, 0)
+        layout.addWidget(Color("green"), 1, 0)
+        layout.addWidget(Color("blue"), 1, 1)
+        layout.addWidget(Color("yellow"), 2, 1)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+
+
+app = QApplication()
+
+window = MainWindow()
+window.show()
+
+app.exec()
+
